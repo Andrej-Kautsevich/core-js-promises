7b8c5b8581911b6ad9c439c06c354991b79f250f
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-04-15 21:50:25 +0300

    feat: complete getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -117,8 +117,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const array = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.forEach((result) => {
+      array.push(result.value ?? null);
+    });
+    return array;
+  });
 }
 
 /**
